5e3a9618a30440aada863ba237a3dd189755d825
SantinaCS <santina.funosas@student.unisg.ch>
2026-05-01 12:10:04 +0200

    j

--- Home.py
+++ Home.py
@@ -418,6 +418,8 @@ with st.spinner("Fetching map data…"):
 def build_map(weather=None, pharmacies=[], doctors=[]):
     m = folium.Map(location=[46.8, 8.2], zoom_start=8, tiles="CartoDB positron", control_scale=True)
     m.fit_bounds([[45.8, 5.9], [47.9, 10.5]])
+    m.options['maxBounds'] = [[45.5, 5.5], [48.2, 10.8]]
+    m.options['maxBoundsViscosity'] = 1.0
     heat_pts = []
     for city, info in STATIONS.items():
         city_vals = all_data.get(city, {})
